a0a02b1b287a8e73e3bdf80a63d3c7c5d478bb81
小川蒼太郎 <ogawasotaro@Sotaro-Ogawa.local>
2021-11-09 13:51:46 +0900

    firstcommit

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,22 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  # POST /books/:book_id/favorites
+  # POST /favorites.json
+  def create
+    if @book = Book.find(params[:book_id])
+      !Favorite.where(book_id: params[:book_id], user_id: current_user.id).exists?
+      @favorite = current_user.favorites.new(:book_id => params[:book_id])
+      @favorite.save
+    end
+  end
+
+
+  # DELETE /books/:book_id/favorites/
+  def destroy
+    if @book = Book.find(params[:book_id])
+      !Favorite.where(book_id: params[:book_id], user_id: current_user.id).exists?
+      @favorite = Favorite.find_by(user_id: current_user, book_id: params[:book_id])
+      @favorite.destroy
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  attachment :profile_image, destroy: false
+  has_many :books
+  has_many :favorites
+  has_many :book_comments
+  validates :name, presence: true, length: {maximum: 10, minimum: 2}
+  validates :introduction, length: {maximum: 50}
+
+   # フォローしている側のUserから見て、フォローされている側のUserを(中間テーブルを介して)集める。参照するカラムは 'follower_id(フォローする側)
+  has_many :relationships, class_name: 'Relationship',foreign_key: "follower_id"
+   #中間テーブル(relationships)を介して「followee」モデルのUser(フォローされた側)「follow_id」を集めることを「followings」と定義
+  has_many :followings, through: :relationships, source: :followee
+
+  # フォローされている側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。参照するカラムは’followee_id’(フォローされる側)
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'followee_id'
+  #中間テーブル(relationships)を介して「user」モデルのUser(フォローする側)「follower_id」を集めることを「followers」と定義
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+ #フォローしているか判定
+  def following?(another_user)
+    self.followings.include?(another_user)
+  end
+
+ #フォローしたときの処理
+  def follow(another_user)
+    unless self == another_user
+      self.relationships.find_or_create_by(followee_id: another_user.id)
+    end
+  end
+
+  #フォローを外すときの処理
+  def unfollow(another_user)
+    unless self == another_user
+      relationship = self.relationships.find_by(followee_id: another_user.id)
+      relationship.destroy
+    end
+  end
+
+end
